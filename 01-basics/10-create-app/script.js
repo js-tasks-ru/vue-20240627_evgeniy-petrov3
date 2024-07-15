88ce2087038c4b301464c9c588b03576681eb813
13zlo <13zloy@mail.ru>
2024-07-15 21:54:51 +0600

    solve create-app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,14 @@
-import { defineComponent, createApp } from 'vue/dist/vue.esm-bundler.js'
+import {defineComponent, createApp} from 'vue/dist/vue.esm-bundler.js';
+
+const App = defineComponent({
+  name: 'Today',
+  setup() {
+    const today = new Date().toLocaleDateString('en-EN', { dateStyle: 'long' });
+    return {
+      today
+    }
+  },
+  template: '<div>Сегодня {{today}}</div>'
+});
+
+createApp(App).mount('#app');
