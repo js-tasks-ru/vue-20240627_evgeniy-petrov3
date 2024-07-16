378f938ffef35fb37c1f15c6d088d2c1d4e3da97
13zlo <13zloy@mail.ru>
2024-07-16 23:06:47 +0600

    solve create-app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -3,12 +3,12 @@ import {defineComponent, createApp} from 'vue/dist/vue.esm-bundler.js';
 const App = defineComponent({
   name: 'Today',
   setup() {
-    const today = new Date().toLocaleDateString('en-EN', { dateStyle: 'long' });
+    const today = new Date().toLocaleDateString(navigator.language, { dateStyle: 'long' });
     return {
       today
     }
   },
-  template: '<div>Сегодня {{today}}</div>'
+  template: '<div>Сегодня {{ today }}</div>'
 });
 
 createApp(App).mount('#app');
--- 01-basics/20-weather/WeatherApp.js
+++ 01-basics/20-weather/WeatherApp.js
@@ -3,27 +3,37 @@ import { getWeatherData, WeatherConditionIcons } from './weather.service.ts'
 
 export default defineComponent({
   name: 'WeatherApp',
+  setup () {
+    const weatherData = getWeatherData();
+    function findIcon(id) {
+        return WeatherConditionIcons[id];
+    }
+    return {
+      data: weatherData,
+      findIcon
+    }
 
+  },
   template: `
     <div>
       <h1 class="title">Погода в Средиземье</h1>
 
       <ul class="weather-list unstyled-list">
-        <li class="weather-card weather-card--night">
+        <li v-for="item in data" class="weather-card weather-card--night">
           <div class="weather-alert">
             <span class="weather-alert__icon">⚠️</span>
             <span class="weather-alert__description">Королевская метеослужба короля Арагорна II: Предвещается наступление сильного шторма.</span>
           </div>
           <div>
             <h2 class="weather-card__name">
-              Гондор
+              {{ item.geographic_name }}
             </h2>
             <div class="weather-card__time">
-              07:17
+              {{ item.current.dt }}
             </div>
           </div>
           <div class="weather-conditions">
-            <div class="weather-conditions__icon" title="thunderstorm with heavy rain">⛈️</div>
+            <div class="weather-conditions__icon" title="thunderstorm with heavy rain">{{ findIcon(item.current.weather.id) }}️</div>
             <div class="weather-conditions__temp">15.0 °C</div>
           </div>
           <div class="weather-details">
@@ -33,15 +43,15 @@ export default defineComponent({
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Влажность, %</div>
-              <div class="weather-details__item-value">90</div>
+              <div class="weather-details__item-value">{{ item.current.humidity }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Облачность, %</div>
-              <div class="weather-details__item-value">100</div>
+              <div class="weather-details__item-value">{{ item.current.clouds }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Ветер, м/с</div>
-              <div class="weather-details__item-value">10.5</div>
+              <div class="weather-details__item-value">{{ item.current.wind_speed }}</div>
             </div>
           </div>
         </li>
