40e5c12eaa896ac3a52c26e68f0d7c69c43ff3c2
13zlo <13zloy@mail.ru>
2024-08-17 18:52:26 +0600

    solve counter

--- 02-basics-2/10-counter/CounterApp.js
+++ 02-basics-2/10-counter/CounterApp.js
@@ -1,9 +1,38 @@
-import { defineComponent } from 'vue'
+import {computed, defineComponent} from 'vue'
+import { ref } from 'vue'
 
 export default defineComponent({
   name: 'CounterApp',
 
-  setup() {},
+  setup() {
+    const min = 0;
+    const max = 5;
+    const count = ref(min);
+    const disabledUp = computed(() => {
+      return count.value === max;
+    })
+    const disabledDown = computed(() => {
+      return count.value === min;
+    })
+    function increment() {
+      if (count.value < max) {
+        count.value++;
+      }
+    }
+    function decrement() {
+      if (count.value > min) {
+        count.value--;
+      }
+    }
+
+    return {
+      count,
+      increment,
+      decrement,
+      disabledUp,
+      disabledDown
+    }
+  },
 
   template: `
     <div class="counter">
@@ -11,15 +40,18 @@ export default defineComponent({
         class="button button--secondary"
         type="button"
         aria-label="Decrement"
-        disabled
+        :disabled="disabledDown"
+        @click="decrement"
       >➖</button>
 
-      <span class="count" data-testid="count">0</span>
+      <span class="count" data-testid="count">{{ count }}</span>
 
       <button
         class="button button--secondary"
         type="button"
         aria-label="Increment"
+        @click="increment"
+        :disabled="disabledUp"
       >➕</button>
     </div>
   `,
