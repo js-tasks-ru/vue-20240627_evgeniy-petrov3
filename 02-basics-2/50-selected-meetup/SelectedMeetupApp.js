ed5890df3d6114ce482175e7449401016cdd36da
13zlo <13zloy@mail.ru>
2024-09-02 22:13:07 +0600

    solve emails and meetups

--- 02-basics-2/50-selected-meetup/SelectedMeetupApp.js
+++ 02-basics-2/50-selected-meetup/SelectedMeetupApp.js
@@ -1,75 +1,80 @@
-import { defineComponent } from 'vue'
-// import { getMeetup } from './meetupsService.ts'
+import {computed, defineComponent, ref, watch} from 'vue'
+import { getMeetup } from './meetupsService.ts'
+import {set} from "@vueuse/core";
 
 export default defineComponent({
   name: 'SelectedMeetupApp',
 
-  setup() {},
+  setup() {
+    const min = 1;
+    const max = 5;
+    const current = ref(1);
+    const input = ref('');
+    const disabledUp = computed(() => {
+      return current.value === max;
+    })
+    const disabledDown = computed(() => {
+      return current.value === min;
+    });
+    const meetUp = ref(getMeetup(min).then(function (res) {
+      meetUp.value = res;
+    }));
+    function setMeetUp(id) {
+      if (current.value >= min && current.value <= max) {
+        getMeetup(id).then(function (result) {
+          meetUp.value = result;
+        })
+        current.value = id;
+      }
+    }
+    function checked(itemId) {
+      return itemId === current.value;
+    }
+    function setCount(id) {
+      current.value = id;
+    }
+    watch(current, (id)=> {
+      setMeetUp(id);
+    })
+
+    return {
+      meetUp,
+      current,
+      disabledUp,
+      disabledDown,
+      input,
+      max,
+      checked,
+      setCount
+    }
+  },
 
   template: `
     <div class="meetup-selector">
       <div class="meetup-selector__control">
-        <button class="button button--secondary" type="button" disabled>Предыдущий</button>
+        <button class="button button--secondary" type="button" :disabled="disabledDown" @click="setCount(current-1)">Предыдущий</button>
 
         <div class="radio-group" role="radiogroup">
-          <div class="radio-group__button">
+          <div class="radio-group__button" v-for="itemId in max">
             <input
-              id="meetup-id-1"
+              :id="'meetup-id-'+ itemId"
               class="radio-group__input"
               type="radio"
               name="meetupId"
-              value="1"
+              value="{{ itemId }}"
+              :checked="checked(itemId)"
+              @change="setCount(itemId)"
             />
-            <label for="meetup-id-1" class="radio-group__label">1</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-2"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="2"
-            />
-            <label for="meetup-id-2" class="radio-group__label">2</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-3"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="3"
-            />
-            <label for="meetup-id-3" class="radio-group__label">3</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-4"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="4"
-            />
-            <label for="meetup-id-4" class="radio-group__label">4</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-5"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="5"
-            />
-            <label for="meetup-id-5" class="radio-group__label">5</label>
+            <label :for="'meetup-id-'+ itemId" class="radio-group__label">{{ itemId }}</label>
           </div>
         </div>
 
-        <button class="button button--secondary" type="button">Следующий</button>
+        <button class="button button--secondary" type="button" :disabled="disabledUp" @click="setCount(current+1)">Следующий</button>
       </div>
 
       <div class="meetup-selector__cover">
         <div class="meetup-cover">
-          <h1 class="meetup-cover__title">Some Meetup Title</h1>
+          <h1 class="meetup-cover__title">{{ meetUp.title }}</h1>
         </div>
       </div>
 
